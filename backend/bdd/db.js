7dfe927e933a4dbe0fba04a4996998746d2b6940
Naila Bouzalmad <neilabouzalmad@gmail.com>
2021-02-25 16:16:39 +0100

    async/await

--- backend/bdd/db.js
+++ backend/bdd/db.js
@@ -1,19 +1,22 @@
 const mySql = require('mysql2')
  
-const con = mySql.createConnection({
+const pool = mySql.createPool({
     host: 'localhost',
     user: 'root',
     port: 8889,
     password: 'root',
     database: 'ecomm-beaute'
 });
-con.connect(function(err) {
-    if (err) throw err;
-    console.log('ok')
 
-    //Création de la table admnistrateur
-   
-    con.query(`CREATE TABLE IF NOT EXISTS admin
+const con = pool.promise()
+
+initDataBase(con)
+
+async function initDataBase(con) {
+
+ 
+
+    await con.query(`CREATE TABLE IF NOT EXISTS admin
                 (id_admin INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
                 lastname VARCHAR(150) NOT NULL, 
                 firstname VARCHAR(150) NOT NULL, 
@@ -21,8 +24,7 @@ con.connect(function(err) {
                 password VARCHAR(250) NOT NULL)`);   
 
       //Création de la table articles
-
-    con.query(`CREATE TABLE IF NOT EXISTS articles
+    await con.query(`CREATE TABLE IF NOT EXISTS articles
                 (id_article INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                  title VARCHAR(250) NOT NULL, 
                  paragraphe MEDIUMTEXT NOT NULL,
@@ -33,12 +35,12 @@ con.connect(function(err) {
       
              //Création de la table catégorie
 
-             con.query(`CREATE TABLE IF NOT EXISTS category 
+             await con.query(`CREATE TABLE IF NOT EXISTS category 
              (id_category INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
               denomination VARCHAR(250) NOT NULL)`);                 
                //Création de la table produits
         
-    con.query(`CREATE TABLE IF NOT EXISTS products
+    await con.query(`CREATE TABLE IF NOT EXISTS products
                (id_product INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                 name VARCHAR(150) NOT NULL, 
                 description TEXT(1000) NOT NULL,
@@ -53,7 +55,7 @@ con.connect(function(err) {
             
               //Création de la table client
     
-      con.query(`CREATE TABLE IF NOT EXISTS customer
+      await con.query(`CREATE TABLE IF NOT EXISTS customer
                   (id_customer INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                   lastname VARCHAR(150) NOT NULL, 
                   firstname VARCHAR(150) NOT NULL, 
@@ -62,26 +64,27 @@ con.connect(function(err) {
 
               //Création de la table favoris
 
-      con.query(`CREATE TABLE IF NOT EXISTS favoris
+      await con.query(`CREATE TABLE IF NOT EXISTS favoris
                  (id_favoris INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                   id_product INT NOT NULL,
                   id_customer INT NOT NULL,
                   FOREIGN KEY (id_product) REFERENCES products(id_product),
                   FOREIGN KEY (id_customer) REFERENCES customer(id_customer) )`)
 
-      con.query(`CREATE TABLE IF NOT EXISTS cart
+      await con.query(`CREATE TABLE IF NOT EXISTS cart
                   (id_cart INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                     ordered BOOLEAN,
                     id_customer INT NOT NULL,
                     FOREIGN KEY (id_customer) REFERENCES customer(id_customer))`)
       
-      con.query(`CREATE TABLE IF NOT EXISTS cartproduct
+      await con.query(`CREATE TABLE IF NOT EXISTS cartproduct
                  (id_cartproduct INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                   quantity DECIMAL(10,2) NOT NULL,
                   id_cart INT,
                   id_product INT,
                   FOREIGN KEY (id_product) REFERENCES products(id_product),
                   FOREIGN KEY (id_cart) REFERENCES cart(id_cart))`)
-  });
+
+  }
 
 module.exports = con
