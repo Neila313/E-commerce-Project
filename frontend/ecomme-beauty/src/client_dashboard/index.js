b4e0c955a7da5e221048455fc2acd7de78ad80fb
Naila Bouzalmad <neilabouzalmad@gmail.com>
2021-03-07 20:34:49 +0100

    dashboard client

--- frontend/ecomme-beauty/src/client_dashboard/index.js
+++ frontend/ecomme-beauty/src/client_dashboard/index.js
@@ -1,96 +1,105 @@
 import React from 'react';
+import HTTP from '../provider/http';
 import Tab from 'react-bootstrap/Tab';
 import Row from 'react-bootstrap/Row';
 import Nav from 'react-bootstrap/Nav';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import CommandeClient from '../commande_client/index'
+
+import './style.css';
+
 // import TabPane from 'react-bootstrap/TabPane'
 
 class DashboardClient extends React.Component {
+	state = {
+		dataUser: [],
+		lastname: '',
+		firstname: '',
+		email: '',
+	};
+
 	logOutSubmit = () => {
 		localStorage.clear();
 		this.props.history.push('/home');
 	};
+
+	componentDidMount() {
+		this.loadCustomer();
+	}
+	loadCustomer() {
+		HTTP.get('/customer/customer').then((res) => {
+			this.setState({ dataUser: res.data });
+			console.log(this.state.dataUser);
+		});
+	}
+
 	render() {
 		return (
-			<div>
-				<Tab.Container id="left-tabs-example" defaultActiveKey="first">
-					<Row>
-						<Col sm={3}>
-							<Nav variant="pills" className="flex-column">
-								<Nav.Item>
-									<Nav.Link eventKey="first">Tableau de bord</Nav.Link>
+			<div className="tabcontainer">
+				<Breadcrumb className="breadcrumb">
+					<Breadcrumb.Item className="breadcrumb-item" href="/home">
+						Accueil
+					</Breadcrumb.Item>
+					<Breadcrumb.Item active>Mon compte</Breadcrumb.Item>
+				</Breadcrumb>
+
+				<Tab.Container id="left-tabs-example" className="tabcontainer" defaultActiveKey="first">
+					<Row className="row">
+						<Col className="col" sm={3}>
+							<Nav variant="link" className="flex-column">
+								<Nav.Item className="nav-item">
+									<Nav.Link className="nav-link" eventKey="first">Tableau de bord
+									<span className="iconeDashboard"></span>
+									</Nav.Link>
 								</Nav.Item>
 								<Nav.Item>
-									<Nav.Link eventKey="second">Commandes</Nav.Link>
+									<Nav.Link className="nav-link" eventKey="second">Commandes
+									<span className="iconeCommande"></span>
+									</Nav.Link>
 								</Nav.Item>
 								<Nav.Item>
-									<Nav.Link eventKey="third">Détails du compte</Nav.Link>
+									<Nav.Link className="nav-link" eventKey="third">Détails du compte
+									<span className="iconeCompte"></span>
+									</Nav.Link>
 								</Nav.Item>
-									<Button className="btn" onClick={this.logOutSubmit.bind(this)}>
-										Sign Out
-									</Button>
-							
+								<Button className="btn2 effect02" onClick={this.logOutSubmit.bind(this)}>
+									<span>Déconnexion</span>
+								</Button>
 							</Nav>
 						</Col>
 						<Col sm={9}>
-							<Tab.Content>
-								<Tab.Pane eventKey="first">
-									<p>
-										Bonjour Utilisateur À partir du tableau de bord de votre compte, vous pouvez
-										visualiser vos commandes récentes, gérer vos adresses de livraison et de
-										facturation ainsi que changer votre mot de passe et les détails de votre compte.
+							<Tab.Content className="tab-content">
+								<Tab.Pane className="tab-pane" eventKey="first">
+									<p className="nameUser">Bienvenue, <span>{this.state.dataUser.firstname} </span> !</p>
+									<p className="welcomeUser">
+										À partir du tableau de bord de votre compte, vous pouvez visualiser vos
+										commandes récentes, gérer vos adresses de livraison et de facturation ainsi que
+										changer votre mot de passe et les détails de votre compte.
 									</p>
 								</Tab.Pane>
-								<Tab.Pane eventKey="second">
+								<Tab.Pane className="tab-pane" eventKey="second">
 									<p>Vos commandes ici</p>
+									<CommandeClient></CommandeClient>
 								</Tab.Pane>
-								<Tab.Pane eventKey="third">
-									<p>Formulaires</p>
-									<Form>
+								<Tab.Pane className="tab-pane" eventKey="third">
+									<Form className="formUser">
 										<Form.Group controlId="formGroupClientLastName">
-											<Form.Label>Last Name</Form.Label>
-											<Form.Control
-												type="lastname"
-												placeholder="Enter your lastname"
-											/>
+											<Form.Label className="form-label">Nom</Form.Label>
+											<Form.Control className="custom" type="lastname"/>
 										</Form.Group>
 										<Form.Group controlId="formGroupClientFirstName">
-											<Form.Label>First Name</Form.Label>
-											<Form.Control
-												type="firstname"
-												placeholder="Enter your firstname"
-											/>
+											<Form.Label>Prénom</Form.Label>
+											<Form.Control className="custom" type="firstname"/>
 										</Form.Group>
 										<Form.Group controlId="formGroupClientEmail">
-											<Form.Label>Email address</Form.Label>
-											<Form.Control
-												type="email"
-												placeholder="Enter email"
-											/>
+											<Form.Label>Email</Form.Label>
+											<Form.Control className="custom" type="email"/>
 										</Form.Group>
-										<p>Changement de mots de passe</p>
-										<br/>
-										<Form.Group controlId="formGroupClientPassword">
-											<Form.Label>Mots de passe actuel</Form.Label>
-											<Form.Control
-												type="password"
-												placeholder="Password"
-											/>
-											<Form.Label>Nouveau mots de passe</Form.Label>
-											<Form.Control
-												type="password"
-												placeholder="Password"
-											/>
-											<Form.Label>Confirmer le nouveau mots de passe</Form.Label>
-											<Form.Control
-												type="password"
-												placeholder="Password"
-											/>
-										</Form.Group>
-										<Button variant="info" type="submit">
-											Enregistrez les modifications
+										<Button className="btn2 effect02" type="submit">
+											<span>Enregistrez les modifications</span>
 										</Button>
 									</Form>
 								</Tab.Pane>
--- frontend/ecomme-beauty/src/commande_client/index.js
+++ frontend/ecomme-beauty/src/commande_client/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
+// import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+// import Image from 'react-bootstrap/Image';
 // import HTTP from '../provider/http';
 
 class CommandeClient extends React.Component {
 	render() {
 		return (
-			<div>Votre commande a bien été validée</div>
+			<div>
+				<Table striped bordered hover size="sm">
+					<thead>
+						<tr>
+							<th>ID Commande</th>
+							<th>Date</th>
+							<th>Statut</th>
+							<th>Total</th>
+							<th>Détail de la commande</th>
+						</tr>
+					</thead>
+					<tbody>
+							<tr>
+								<td>id commande</td>
+								<td>date</td>
+								<td>statut</td>
+								<td>total</td>
+								<td>boutton détails</td>
+							</tr>
+					</tbody>
+				</Table>
+			</div>
 		);
 	}
 }
